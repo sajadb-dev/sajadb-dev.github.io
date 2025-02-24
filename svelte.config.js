4d328a2fa93cfcded8d23d183d27a5267a750df7
TheMoonKnight8 <48894025+sajadb-dev@users.noreply.github.com>
2025-02-25 01:49:28 +0330

    minor changes

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,10 @@ import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
+
+const dev = process.env.NODE_ENV === 'development';
+const base = dev ? '' : `/${process.env.BASE_PATH || 'game-dev-website'}`; // Set the correct repo name
+
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
@@ -16,7 +20,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base
 		}
 	}
 };
