5e61e2dddfbdc2acc117d80d63df03001efcbdbd
TheMoonKnight8 <48894025+sajadb-dev@users.noreply.github.com>
2025-02-25 02:52:22 +0330

    changes

--- svelte.config.js
+++ svelte.config.js
@@ -2,10 +2,6 @@ import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
-
-const dev = process.env.NODE_ENV === 'development';
-const base = dev ? '' : '';
-
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
@@ -18,14 +14,11 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base
+			base: ''
 		},
 		prerender: {
 			entries: ['*']
 		 },
-		router: {
-			hash: true 
-		}
 	}
 };
 
